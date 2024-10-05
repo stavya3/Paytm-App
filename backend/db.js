f6244599815e9a3b3a48d385d74b75adff3ec43b
stavya3 <stavyakc03@gmail.com>
2024-10-05 07:42:22 +0530

    Created the schema for accounts

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://stavya:shanu123@cluster0.g4jei.mongodb.net/") // enter the db name later
+mongoose.connect("mongodb+srv://stavya:shanu123@cluster0.g4jei.mongodb.net/paytm") // enter the db name later
 
 const userSchema = new mongoose.Schema({
     username: String,
@@ -9,8 +9,24 @@ const userSchema = new mongoose.Schema({
 
 });
 
-const User = mongoose.model("User", userSchema);
 
+
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // creating a reference to User model, kinda like foreign key
+        ref: User,
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    },
+});
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = {
-    User
+    User,
+    Account,
 }
