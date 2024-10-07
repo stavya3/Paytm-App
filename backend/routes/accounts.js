23a014cce1d4b108db20bd529c4d4d788c0a6cf5
stavya3 <stavyakc03@gmail.com>
2024-10-07 08:58:48 +0530

    Added a transaction to for payments and updating balance

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -17,6 +17,38 @@ router.get('/balance', authMiddleware, async (req, res) => {
 
 router.post('transfer', authMiddleware, async (req,res) => {
 
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    
+    const { amount, to } = req.body;
+
+    const account = await Account.findOne({
+        userId: req.userId
+    }).session(session); // This means fetch the accounts within the transaction
+
+    if (!account || account.balance < amount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Insufficient Balance"
+        })
+
+    }
+    const toAccount = await Account.findOne({
+        userId: to
+    }).session(session);
+    if (!toAccount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Invalid Account"
+        })
+    }
+    // Making the transfer
+    await Account.updateOne({userId: req.userId}, {$inc: { balance: -amount}}).session(session);
+    await Account.updateOne({userId: to}, {$inc: { balance: amount}}).session(session);
+    await session.commitTransaction();
+    res.json({
+        message: "Transfer Successful"
+    })
     
 });
 
