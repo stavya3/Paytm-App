383310e9d11ee23c73747ce63bf5ea9c2c66ba72
stavya3 <stavyakc03@gmail.com>
2024-10-07 08:06:06 +0530

    Added the route to get the amount in account

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const { Account } = require("../db");
+const { authMiddleware } = require("../middleware");
 
 const router = express.Router();
+
+
+router.get('/balance', authMiddleware, async (req, res) => {
+     const account = await Account.findOne({
+        userId: req.userId
+     });
+
+     res.json({
+        balance: account.balance
+     })
+});
+
+router.post('transfer', authMiddleware, async (req,res) => {
+
+    
+});
 
 module.exports = router;
