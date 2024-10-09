19ccdb3f60c4853faffc0fcfe3da2fdd15f6d26d
stavya3 <stavyakc03@gmail.com>
2024-10-09 07:38:32 +0530

    Added and styled the signup and the signin pages

---
+++ frontend/src/pages/Signin.jsx
@@ -0,0 +1,25 @@
+import { Heading } from '../components/Heading'
+import { SubHeading } from '../components/SubHeading'
+import { InputBox } from '../components/InputBox'
+import { Button } from '../components/Button'
+import { BottomWarning } from '../components/BottomWarning'
+
+
+
+
+export const Signin = () => {
+    return <div className="bg-slate-300 h-screen flex justify-center">
+        <div className="flex flex-col justify-center">
+            <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+                <Heading label={"Sign In"} />
+                <SubHeading label={"Enter your credentials to access your account"} />
+                <InputBox placeholder="stavya@gmail.com" label={"Email"} />
+                <InputBox placeholder="123456" label={"Password"} />
+                <div>
+                    <Button label={"Sign in"} />
+                </div>
+                <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
+            </div>
+        </div>
+    </div>
+};
