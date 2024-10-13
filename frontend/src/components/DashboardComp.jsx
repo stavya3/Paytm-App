6c3f86a037b55df99d8168c93428c6807237bfbc
stavya3 <stavyakc03@gmail.com>
2024-10-13 16:01:51 +0530

    Added the component for the sidebar and dashboard, also layed the components out

---
+++ frontend/src/components/DashboardComp.jsx
@@ -0,0 +1,7 @@
+
+
+
+
+export const DashboardComp = () => {
+    return <div className="bg-white rounded-lg pb-4 shadow h-[200vh]"></div>
+}
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Sidebar = () => {
+    return (
+        <div>
+            <div>
+                {/* TODO: main sidebar content */}
+            </div>
+            <div>
+                {/* TODO: maybe logout */}
+            </div>
+        </div>
+    );
+}
