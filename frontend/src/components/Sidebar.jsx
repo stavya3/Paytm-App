530c7bac1ee7e9bfaa5e53d047c78c243f21df9e
stavya3 <stavyakc03@gmail.com>
2024-10-14 16:51:22 +0530

    Positioned and styles the sidebar comp, also added the account toggle button

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { AccountToggle } from "./AccountToggle";
 
 export const Sidebar = () => {
     return (
         <div>
-            <div>
-                {/* TODO: main sidebar content */}
+            <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
+                <AccountToggle />
             </div>
             <div>
                 {/* TODO: maybe logout */}
